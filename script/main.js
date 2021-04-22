04a512357a187cc968b2519a6d8f855dc197ffdb
WojciechLisCode <wlis58372+code@gmail.com>
2021-04-22 16:13:47 +0200

    last animation

--- script/main.js
+++ script/main.js
@@ -4,15 +4,19 @@ const body = document.querySelector("#body");
 
 button2.onclick = function(event) {
     if (description.classList.contains("descriptionHidden")) {
+
+        body.classList.add("expanded")
+
         description.classList.add("descriptionTransition");
         description.clientWidth;
         description.classList.remove("descriptionHidden")
-
+        
         console.log('hidden to visible')
     } else {
         description.classList.add("descriptionTransition");
         description.classList.add("descriptionHidden")        
         console.log("visibletohidden")
+        body.classList.remove("expanded")
     }
 
     description.addEventListener('transitionend', function() {
